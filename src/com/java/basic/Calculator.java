36f0ae9326d7c04d843e34debd9ce0244fbdb740
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-02 20:00:09 +0530

    Added: Welcome Message

--- src/com/java/basic/Calculator.java
+++ src/com/java/basic/Calculator.java
@@ -4,7 +4,16 @@ public class Calculator {
 
 	public static void main(String[] args) {
 	System.out.println("Welcome to Calculator!!!");
-
+     int num1=10;
+     int num2=20;
+     int sum=num1+num2;
+    
+     int mul=num1*num2;
+     int div=num1/num2;
+     System.out.println("Sum is: "+sum);
+     
+     System.out.println("Mul is: "+mul);
+     System.out.println("Div is: "+div);
 	}
 
 }
