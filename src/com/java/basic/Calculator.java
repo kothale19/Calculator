74c1d4644a39d16109be2af5b9e56b8840d467c1
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-02 21:00:27 +0530

    Added: substraction logic

--- src/com/java/basic/Calculator.java
+++ src/com/java/basic/Calculator.java
@@ -7,13 +7,15 @@ public class Calculator {
      int num1=10;
      int num2=20;
      int sum=num1+num2;
-    
+     int sub=num1-num2;
      int mul=num1*num2;
      int div=num1/num2;
+    
      System.out.println("Sum is: "+sum);
-     
+     System.out.println("Sub is: "+sub);
      System.out.println("Mul is: "+mul);
      System.out.println("Div is: "+div);
+     
 	}
 
 }
